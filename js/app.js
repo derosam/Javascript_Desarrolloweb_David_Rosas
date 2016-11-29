0d0beda40d6f6522df13de03303219dcdc6ab00a
derosam <davideduardorosasmartinez@davidmac.local>
2016-11-29 15:51:55 -0500

    Ingreso numerico

--- js/app.js
+++ js/app.js
@@ -1,20 +1,112 @@
-var myApp = ( function(){
+var calculadora = ( function(){
  
-  var foo = 'Module Pattern';
-  var bar = 'ver 1.0';
- 
+    var numero="";
+    var tag="";
+    var operandos=[];
+    var operadores=[];
+    
+    function ingreso(tag){
+        switch(tag) {
+    case "1":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }        
+        break;
+    case "2":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "3":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "4":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "5":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "6":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "7":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "8":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "9":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "0":
+        if(numero.length<9){
+        numero+=tag;        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;            
+    case "punto":
+        if(numero.length<9){
+        numero+=".";        
+        document.getElementById("display").innerHTML = numero;   
+        }
+        break;
+    case "signo":
+        if(numero.charAt(0)=="-"){
+            numero=numero.slice(1);
+            document.getElementById("display").innerHTML = numero; 
+        }else{
+            numero="-"+numero;
+            document.getElementById("display").innerHTML = numero;       
+        }        
+        break;
+                
+}
+    }
+
     
   return {
-    myMessage: function(){
-      return foo + ' ' + bar;
-    },
+elemento: function (e){
+if (e.target)
+  	  tag = e.target.getAttribute('ID');
+      ingreso(tag);
+}       
       
-   sum: function( param1, param2 ){
-    return param1 + param2;
-  }
+
   
   }
 })();
  
-console.log( myApp.myMessage() ); // Module Pattern ver 1.0
-console.log( myApp.sum( 10, 5 ) ); // myApp.sum is not a function. sum es privada
+
+//console.log( calculadora.sum( 10, 5 ) ); // myApp.sum is not a function. sum es privada
+
+/*elemento: function (e){
+  if (e.srcElement)
+	  tag = e.srcElement.getAttribute('ID') ;
+  else if (e.target)
+  	  tag = e.target.getAttribute('ID');
+ 
+  alert("El elemento selecionado ha sido " + tag);
+}*/
